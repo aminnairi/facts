930cd2de8f0ab21abc997170632b1d800b4e3558
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-11-17 21:44:35 +0100

    initial commit

---
+++ packages/examples/sqlite/snapshot.ts
@@ -0,0 +1,98 @@
+import { randomUUID } from "crypto";
+import { SqliteFactStore } from "@aminnairi/facts";
+
+interface TodoAddedV1Fact {
+  name: "todo-added"
+  identifier: string
+  sequence: number
+  version: 1
+  aggregate: "todo"
+  date: Date
+  aggregateIdentifier: string
+  data: {
+    name: string
+    done: boolean
+  }
+}
+
+interface TodoRemovedV1Fact {
+  name: "todo-removed"
+  date: Date
+  identifier: string
+  sequence: number
+  version: 1
+  aggregate: "todo"
+  aggregateIdentifier: string
+  data: null
+}
+
+interface TodoSnapshotV1Fact {
+  name: "todo-snapshot"
+  date: Date
+  identifier: string
+  sequence: number
+  version: 1
+  aggregate: "todo"
+  aggregateIdentifier: string
+  data: {
+    name: string
+    done: boolean
+  }
+}
+
+type TodoFact =
+  | TodoAddedV1Fact
+  | TodoRemovedV1Fact
+  | TodoSnapshotV1Fact
+
+const factStore = new SqliteFactStore<TodoFact>(":memory:")
+const aggregateIdentifier = randomUUID()
+
+await factStore.save({
+  aggregate: "todo",
+  aggregateIdentifier,
+  identifier: randomUUID(),
+  date: new Date(),
+  name: "todo-added",
+  sequence: 0,
+  version: 1,
+  data: {
+    done: false,
+    name: "Do the dishes"
+  }
+})
+
+await factStore.save({
+  aggregate: "todo",
+  aggregateIdentifier,
+  identifier: randomUUID(),
+  date: new Date(),
+  name: "todo-snapshot",
+  sequence: 1,
+  version: 1,
+  data: {
+    name: "Do the dishes",
+    done: false
+  }
+})
+
+await factStore.save({
+  aggregate: "todo",
+  aggregateIdentifier,
+  date: new Date(),
+  identifier: randomUUID(),
+  name: "todo-removed",
+  sequence: 2,
+  version: 1,
+  data: null
+})
+
+const facts = await factStore.findFromLast(fact => {
+  return fact.name === "todo-snapshot"
+})
+
+console.log("List of facts stored")
+
+for (const fact of facts) {
+  console.log(fact)
+}
