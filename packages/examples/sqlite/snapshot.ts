621cd6efcd95d6082f00578603c9a11a4703e0ca
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-11-22 15:55:36 +0100

    udpated the examples

--- packages/examples/sqlite/snapshot.ts
+++ packages/examples/sqlite/snapshot.ts
@@ -1,57 +1,71 @@
 import { randomUUID } from "crypto";
-import { SqliteFactStore } from "@aminnairi/facts";
+import { FactShape, ParseError, SqliteFactStore } from "@aminnairi/facts";
+import { z, ZodType } from "zod"
 
-interface TodoAddedV1Fact {
-  name: "todo-added"
-  identifier: string
-  position: number
-  version: 1
-  stream: {
-    name: "todo"
-    identifier: string
-  }
-  date: Date
-  payload: {
-    name: string
-    done: boolean
-  }
-}
+const todoAddedV1FactSchema = z.object({
+  name: z.literal("todo-added"),
+  identifier: z.string(),
+  position: z.number(),
+  version: z.literal(1),
+  date: z.coerce.date(),
+  stream: z.object({
+    name: z.literal("todo"),
+    identifier: z.string()
+  }),
+  payload: z.object({
+    name: z.string(),
+    done: z.boolean()
+  })
+}) satisfies ZodType<FactShape>
 
-interface TodoRemovedV1Fact {
-  name: "todo-removed"
-  date: Date
-  identifier: string
-  position: number
-  version: 1
-  stream: {
-    name: "todo"
-    identifier: string
-  }
-  payload: null
-}
+const todoRemovedV1FactSchema = z.object({
+  name: z.literal("todo-removed"),
+  date: z.coerce.date(),
+  identifier: z.string(),
+  position: z.number(),
+  version: z.literal(1),
+  payload: z.null(),
+  stream: z.object({
+    name: z.literal("todo"),
+    identifier: z.string()
+  })
+}) satisfies ZodType<FactShape>
 
-interface TodoSnapshotV1Fact {
-  name: "todo-snapshot"
-  date: Date
-  identifier: string
-  position: number
-  version: 1
-  stream: {
-    name: "todo"
-    identifier: string
-  }
-  payload: {
-    name: string
-    done: boolean
-  }
-}
+const todoSnapshotV1FactSchema = z.object({
+  name: z.literal("todo-snapshot"),
+  date: z.coerce.date(),
+  identifier: z.string(),
+  position: z.number(),
+  version: z.literal(1),
+  stream: z.object({
+    name: z.literal("todo"),
+    identifier: z.string()
+  }),
+  payload: z.object({
+    name: z.string(),
+    done: z.boolean()
+  })
+}) satisfies ZodType<FactShape>
 
-type TodoFact =
-  | TodoAddedV1Fact
-  | TodoRemovedV1Fact
-  | TodoSnapshotV1Fact
+const todoFactSchema = z.union([
+  todoAddedV1FactSchema,
+  todoRemovedV1FactSchema,
+  todoSnapshotV1FactSchema
+])
 
-const factStore = new SqliteFactStore<TodoFact>(":memory:")
+type TodoFact = z.infer<typeof todoFactSchema>
+
+const factStore = SqliteFactStore.for<TodoFact>(":memory:", {
+  parser: fact => {
+    const validation = todoFactSchema.safeParse(fact)
+
+    if (validation.success) {
+      return validation.data
+    }
+
+    return new ParseError(validation.error.message)
+  }
+})
 const streamIdentifier = randomUUID()
 
 await factStore.save({
@@ -102,6 +116,11 @@ await factStore.save({
 const facts = await factStore.findFromLast(fact => {
   return fact.name === "todo-snapshot"
 })
+
+if (facts instanceof Error) {
+  console.error(facts)
+  process.exit(1)
+}
 
 console.log("List of facts stored")
 
--- packages/examples/sqlite/store.ts
+++ packages/examples/sqlite/store.ts
@@ -1,40 +1,55 @@
 import { randomUUID } from "crypto";
-import { SqliteFactStore } from "@aminnairi/facts";
+import { FactShape, ParseError, SqliteFactStore } from "@aminnairi/facts";
+import { z, ZodType } from "zod"
 
-interface TodoAddedV1Fact {
-  name: "todo-added"
-  identifier: string
-  position: number
-  version: 1
-  stream: {
-    name: "todo"
-    identifier: string
+const todoAddedV1FactSchema = z.object({
+  name: z.literal("todo-added"),
+  identifier: z.string(),
+  position: z.number(),
+  version: z.literal(1),
+  date: z.coerce.date(),
+  stream: z.object({
+    name: z.literal("todo"),
+    identifier: z.string()
+  }),
+  payload: z.object({
+    name: z.string(),
+    done: z.boolean()
+  })
+}) satisfies ZodType<FactShape>
+
+const todoRemovedV1FactSchema = z.object({
+  name: z.literal("todo-removed"),
+  date: z.coerce.date(),
+  identifier: z.string(),
+  position: z.number(),
+  version: z.literal(1),
+  payload: z.null(),
+  stream: z.object({
+    name: z.literal("todo"),
+    identifier: z.string()
+  })
+}) satisfies ZodType<FactShape>
+
+const todoFactSchema = z.union([
+  todoAddedV1FactSchema,
+  todoRemovedV1FactSchema,
+])
+
+type TodoFact = z.infer<typeof todoFactSchema>
+
+const factStore = SqliteFactStore.for<TodoFact>(":memory:", {
+  parser: fact => {
+    const validation = todoFactSchema.safeParse(fact)
+
+    if (validation.success) {
+      return validation.data
+    }
+
+    return new ParseError(validation.error.message)
   }
-  date: Date
-  payload: {
-    name: string
-    done: boolean
-  }
-}
+})
 
-interface TodoRemovedV1Fact {
-  name: "todo-removed"
-  date: Date
-  identifier: string
-  position: number
-  version: 1
-  stream: {
-    name: "todo"
-    identifier: string
-  }
-  payload: null
-}
-
-type TodoFact =
-  | TodoAddedV1Fact
-  | TodoRemovedV1Fact
-
-const factStore = new SqliteFactStore<TodoFact>(":memory:")
 const streamIdentifier = randomUUID()
 
 await factStore.save({
@@ -67,6 +82,11 @@ await factStore.save({
 })
 
 const facts = await factStore.find()
+
+if (facts instanceof Error) {
+  console.error(facts)
+  process.exit(1)
+}
 
 console.log("List of facts stored")
 
