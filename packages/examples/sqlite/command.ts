e892fa444e772fd2259757594fab352e5f521f3f
aminnairi <18418459+aminnairi@users.noreply.github.com>
2026-03-02 22:17:12 +0100

    updated the sqlite command example with regards to the recent deletion of the command interface

--- packages/examples/sqlite/command.ts
+++ packages/examples/sqlite/command.ts
@@ -1,4 +1,4 @@
-import { type FactShape, MemoryCommand, SqliteFactStore, ParseError } from "@aminnairi/facts"
+import { type FactShape, type FactStore, SqliteFactStore, ParseError } from "@aminnairi/facts"
 import { randomUUID } from "crypto"
 import { z, ZodType } from "zod";
 
@@ -34,9 +34,41 @@ const todoFactSchema = z.union([
 
 type TodoAddedV1Fact = z.infer<typeof todoAddedV1FactSchema>;
 type TodoRemovedV1Fact = z.infer<typeof todoRemovedV1FactSchema>;
+type TodoFact = TodoAddedV1Fact | TodoRemovedV1Fact;
 
-const addTodoCommand = new MemoryCommand<TodoAddedV1Fact>
-const removeTodoCommand = new MemoryCommand<TodoRemovedV1Fact>
+class AddTodoCommand {
+  constructor(private readonly store: FactStore<TodoFact>) {}
+
+  async execute(name: string, done: boolean) {
+    return this.store.save({
+      name: "todo-added",
+      date: new Date(),
+      identifier: randomUUID(),
+      position: 0,
+      version: 1,
+      streamName: "todo",
+      streamIdentifier: randomUUID(),
+      payload: { name, done }
+    })
+  }
+}
+
+class RemoveTodoCommand {
+  constructor(private readonly store: FactStore<TodoFact>) {}
+
+  async execute(streamIdentifier: string) {
+    return this.store.save({
+      name: "todo-removed",
+      date: new Date(),
+      identifier: randomUUID(),
+      position: 0,
+      version: 1,
+      streamName: "todo",
+      streamIdentifier,
+      payload: null
+    })
+  }
+}
 
 const factStore = SqliteFactStore.for(":memory:", {
   parser: fact => {
@@ -50,38 +82,30 @@ const factStore = SqliteFactStore.for(":memory:", {
   }
 })
 
-factStore.registerCommand(addTodoCommand)
-factStore.registerCommand(removeTodoCommand)
+const addTodoCommand = new AddTodoCommand(factStore)
+const removeTodoCommand = new RemoveTodoCommand(factStore)
 
-let error = await addTodoCommand.send({
-  name: "todo-added",
-  date: new Date(),
-  identifier: randomUUID(),
-  position: 0,
-  version: 1,
-  streamName: "todo",
-  streamIdentifier: randomUUID(),
-  payload: {
-    name: "Do the dishes",
-    done: true
-  }
-})
+let error = await addTodoCommand.execute("Do the dishes", true)
 
 if (error instanceof Error) {
   console.error(error)
   process.exit(1)
 }
 
-error = await removeTodoCommand.send({
-  name: "todo-removed",
+const streamIdentifier = randomUUID()
+
+await factStore.save({
+  name: "todo-added",
   date: new Date(),
   identifier: randomUUID(),
   position: 0,
   version: 1,
   streamName: "todo",
-  streamIdentifier: randomUUID(),
-  payload: null
+  streamIdentifier,
+  payload: { name: "Buy milk", done: false }
 })
+
+error = await removeTodoCommand.execute(streamIdentifier)
 
 if (error instanceof Error) {
   console.error(error)
